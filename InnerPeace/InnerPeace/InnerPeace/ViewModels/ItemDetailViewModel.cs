aa1d8875570eccaa2693837c5264c21e9997bfef
Ram Beer Singh <rsingh.hcl@gmail.com>
2019-03-29 10:36:19 +0530

    First Commit

---
+++ InnerPeace/InnerPeace/InnerPeace/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using InnerPeace.Models;
+
+namespace InnerPeace.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
